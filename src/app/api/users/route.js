88e02c52e90db7090d43ac86b06eea91ce0feb13
Okimchi <basilio.miko123@gmail.com>
2025-05-04 03:35:54 +0800

    added register route controller

---
+++ src/app/api/users/route.js
@@ -0,0 +1,34 @@
+import connectDB from "@/lib/database";
+import User from "@/models/User";
+import { NextResponse } from "next/server";
+import bcrypt from 'bcryptjs';
+
+export async function POST(request){
+    try {
+        await connectDB()
+
+        const {firstname, lastname, email, password} = await request.json()
+
+        const existingEmail = await User.findOne({email})
+        if(existingEmail){
+            return NextResponse.json({message:"Email is already used"}, {status:400})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = new User(
+            {
+            firstname, 
+            lastname, 
+            email, 
+            password: hashedPassword,
+        })
+
+        await newUser.save()
+
+        return NextResponse.json(newUser, {status:201})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({message:"Server Error"}, {status: 500})
+    }
+}
