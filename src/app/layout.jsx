00095fcba3b940fbdf9924cd658e138efefd0fa0
Okimchi <basilio.miko123@gmail.com>
2025-05-05 23:56:38 +0800

    added toaster on main layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import { inter } from "@/components/ui/fonts";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        {children}
+        <Toaster />
+      </body>
     </html>
-  )
+  );
 }
