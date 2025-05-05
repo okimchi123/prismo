d5817e6de206ab3e93c1bfea1e974e181093352e
Okimchi <basilio.miko123@gmail.com>
2025-05-05 23:57:17 +0800

    added logout on dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,42 @@
-export default function Page(){
-    return(
-        <main>
-            <h1>Dashboard</h1>
-        </main>
-    )
+"use client";
+import { useState, useEffect } from "react";
+import { userAuth, logout } from "@/services/auth";
+import { Button } from "@/components/ui/button";
+import { useRouter } from 'next/navigation';
+import { toast } from "sonner";
+
+export default function Page() {
+  const [user, setUser] = useState(null)
+  const router = useRouter()
+
+  const handleLogout = async () => {
+    await logout();
+    router.push('/?loggedout=true')
+  }
+
+  const handleToast = () =>{
+    toast('Hello');
+  }
+
+  useEffect(() => {
+    async function loadUser() {
+      try {
+        const data = await userAuth()
+        setUser(data?.user)
+        console.log(data)
+      } catch (error) {
+        router.push('/')
+      }
+    }
+    loadUser();
+  }, []);
+
+  return (
+    <main>
+      <h1>Dashboard</h1>
+      <p>{user?.email}</p>
+      <Button onClick={handleToast}> Toast here </Button>
+      <Button onClick={handleLogout}>Log out</Button>
+    </main>
+  );
 }
