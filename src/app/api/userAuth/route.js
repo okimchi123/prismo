95f19c905ca98fa962520a32a1e66ee153420be8
Okimchi <basilio.miko123@gmail.com>
2025-05-05 23:56:16 +0800

    added user auth

---
+++ src/app/api/userAuth/route.js
@@ -0,0 +1,14 @@
+import { cookies } from "next/headers";
+import { verifyToken } from "@/lib/jwt";
+import { NextResponse } from "next/server";
+
+export async function GET(params){
+    const token = (await cookies()).get('token')?.value;
+    const user = verifyToken(token)
+
+    if(!user){
+        return NextResponse.json({message:'Unauthorized'}, {status: 401})
+    }
+
+    return NextResponse.json({message: 'Authorized access', user})
+}   
--- src/services/auth.js
+++ src/services/auth.js
@@ -14,6 +14,28 @@ export async function login(data){
         const response = await axios.post('/api/auth/login', data) 
         return response.data;
     } catch (error) {
+
         console.error(error)
     }
 }   
+
+export async function userAuth(){
+    try {
+        const response = await axios.get('/api/userAuth')
+        return response.data;
+    } catch (error) {
+        if(error.response?.status === 401){
+            console.error(error.data.message)
+        }else{
+            console.error(error)
+        }
+    }
+}
+
+export async function logout() {
+    try {
+        const response = await axios.post('api/auth/logout')
+    } catch (error) {
+        console.error(error)
+    }
+}
