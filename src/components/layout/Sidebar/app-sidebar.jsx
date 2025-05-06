30a3409faaae811532bb276b75f1d32795b8436a
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-05-06 16:33:09 +0800

    added sidebar and dropdown ui component

---
+++ src/components/layout/Sidebar/app-sidebar.jsx
@@ -0,0 +1,93 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+} from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import {
+  ChevronUp,
+  User2,
+} from "lucide-react";
+import { items } from "@/models/navItems";
+import { SearchInput } from "@/components/ui/input";
+import { logout } from "@/services/auth";
+const navItems = items;
+
+
+export function AppSidebar() {
+  const router = useRouter()
+  const handleLogout = async () => {
+    await logout();
+    router.push('/?loggedout=true')
+  }
+
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-lg mb-2 select-none">Mikosystem</SidebarGroupLabel>
+          <SearchInput type="text" placeholder="Search" className="mb-4" />
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {navItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild className="">
+                <SidebarMenuButton>
+                  <User2 /> Username
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                 onClick={handleLogout}
+                >
+                  <span
+                 
+                  >Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
---
+++ src/models/navItems.js
@@ -0,0 +1,19 @@
+import {
+    Calendar,
+    Home,
+    Users,
+  } from "lucide-react";
+
+  export const items = [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: Home,
+    },
+    {
+      title: "Friends",
+      url: "#",
+      icon: Users,
+    },
+  ];
+  
