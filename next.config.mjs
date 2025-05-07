6ae8809f479d1ca68978172476b22e694a88d2e8
Okimchi <basilio.miko123@gmail.com>
2025-05-08 01:38:42 +0800

    added env in next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      JWT_SECRET: process.env.JWT_SECRET,
+    },
+  };
+  
+  export default nextConfig;
+  
