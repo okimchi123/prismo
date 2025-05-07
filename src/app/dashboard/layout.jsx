a912f8ddfa79cfb3ea905222898ff7efe0277779
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-05-07 16:28:27 +0800

    added zustand user fetch

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,7 +1,28 @@
+'use client'
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/layout/Sidebar/app-sidebar"
+import { useEffect } from "react";
+import { userAuth, logout } from "@/services/auth";
+import { useRouter } from 'next/navigation';
+import { storeUser } from "@/hooks/state";
 
 export default function Layout({ children }) {
+  const router = useRouter()
+  const setUser = storeUser((state) => state.setUser);
+
+  useEffect(() => {
+    async function loadUser() {
+      try {
+        const data = await userAuth()
+        setUser(data.user)
+        console.log(data.user)
+      } catch (error) {
+        router.push('/')
+      }
+    }
+    loadUser();
+  }, []);
+
   return (
     <SidebarProvider>
       <AppSidebar />
