0d455d6e0099150bfb92b0a9e3249f6152ee6e8d
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-05-08 16:00:48 +0800

    fixed sidebar

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -459,7 +459,8 @@ const sidebarMenuButtonVariants = cva(
         default: "hover:bg-sidebar-accent hover:text-sidebar-accent-foreground",
         outline:
           "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]",
-      },
+        normal:"hover:bg-pink-100 py-5 px-5 transition-all hover:text-none",
+        },
       size: {
         default: "h-8 text-sm",
         sm: "h-7 text-xs",
