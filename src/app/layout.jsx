e5f05d621b5e0d9fdb91f1dfa71550cfd830c589
Okimchi <basilio.miko123@gmail.com>
2025-05-11 20:19:56 +0800

    added client side auth

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,30 @@
+'use client'
 import "@/styles/globals.css";
 import { inter } from "@/components/ui/fonts";
 import { Toaster } from "@/components/ui/sonner";
+import { useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { auth } from "@/lib/firebase";
 
 export default function RootLayout({ children }) {
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("user:", user)  
+        if (pathname === "/" || pathname === "/register") {
+          router.push("/dashboard"); 
+          console.log("eyowwwww")
+        }
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
