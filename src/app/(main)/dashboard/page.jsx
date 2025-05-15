0f07718aca404cc97cae2e704a810bb7226dc246
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-05-15 16:22:34 +0800

    fixed news feed ui, added post button for mobile

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -1,17 +1,39 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { useState } from "react";
+import { storeUser } from "@/hooks/state";
+import PostModal from "@/components/layout/modal/post";
 
 export default function Page() {
-  const handleToast = () =>{
-    toast.warning('Hello');
+  const user = storeUser((state) => state.user);
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
   }
 
-
   return (
-    <main>
-      <h1>Dashboard</h1>
-      <Button onClick={handleToast}> Toast here </Button>
+    <main className="flex flex-col items-center">
+      <PostModal 
+      isOpen={isOpen} 
+      onClose={() => setIsOpen(false)}
+      />
+
+      <section className="w-full flex items-center justify-center rounded-lg gap-2 bg-white px-2 py-3">
+        <figure className="w-10 h-10 relative">
+          <Image src="/finn.jpg" fill alt="profile_pic" className="object-cover rounded-md" />
+        </figure>
+        <Button 
+        onClick={() => setIsOpen(isOpen => !isOpen)}
+        className="bg-white text-gray-500 font-normal gray-bg w-[80%] flex justify-start py-5 active:scale-98 transition-transform"
+        >
+           What's on your mind {user.firstname}? 
+        </Button>
+      </section>
+
     </main>
   );
 }
