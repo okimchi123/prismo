339755387e9f67a2b5d20c7cc7af7f919570b2d4
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-05-15 11:24:46 +0800

    removed loading on login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -47,8 +47,6 @@ export default function Page() {
     }
   }, [user]);
 
-   const loadingAuth = useAuthRedirect("login");
-   if (loadingAuth) return <Loading />;
   return (
     <main className="flex flex-col items-center w-full h-screen gap-2">
       <form
