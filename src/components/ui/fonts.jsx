3d782d6c19babadaec2e2ebf12477caea9d2f9f4
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-05-15 11:26:56 +0800

    added font for the website name

--- src/components/ui/fonts.jsx
+++ src/components/ui/fonts.jsx
@@ -1,3 +1,4 @@
-import { Inter } from 'next/font/google';
+import { Inter, Darumadrop_One } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
+export const darumadrop_one = Darumadrop_One({ subsets: ['latin'], weight:"400" });
