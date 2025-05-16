6398810e7df4e181813f12b0e12e09db24354913
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-05-16 12:00:52 +0800

    updated post modal upto textarea

--- src/components/layout/modal/post.jsx
+++ src/components/layout/modal/post.jsx
@@ -1,17 +1,43 @@
-'use client'
-import { ArrowLeft } from "lucide-react"
-export default function PostModal({isOpen, onClose}){
+"use client";
+import { ChevronLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { storeUser } from "@/hooks/state";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+import clsx from "clsx";
 
-    return (
-        isOpen && (
-          <main className="absolute w-full top-0 h-screen z-100 flex justify-center items-center bg-white">
-            <ArrowLeft className="absolute left-3 top-3"
-            onClick={onClose}
-            />
-            <form action="" className="bg-white w-[30%] h-[50%]">
-                <h1>Post here</h1>
-            </form>
-        </main>  
-        )
+export default function PostModal({ isOpen, onClose }) {
+  const disabledBtn = "bg-gray-400";
+  const user = storeUser((state) => state.user);
+
+  return (
+    isOpen && (
+      <main className="absolute w-full top-0 h-screen z-100 flex justify-center items-start bg-white">
+        <form action="" className="flex flex-col w-full justify-start">
+          <section className="flex items-center p-2 justify-between border-b-1">
+            <div className="flex items-center gap-4">
+              <ChevronLeft onClick={onClose} />
+              <h1>Create post</h1>
+            </div>
+            <Button className={clsx(disabledBtn)}> POST </Button>
+          </section>
+          <section className="px-3 py-2 flex flex-col">
+            <div className="flex gap-2 mb-2">
+              <figure className="w-12 h-12 relative">
+                <Image
+                  src="/finn.jpg"
+                  fill
+                  alt="profile_pic"
+                  className="object-cover rounded-md"
+                />
+              </figure>
+              <span className="text-[14px] font-semibold text-black"> {user.firstname} {user.lastname} </span>
+            </div>
+            <Textarea className="" placeholder="What's on your mind?" />
+            <Image src="" />
+          </section>
+        </form>
+      </main>
     )
+  );
 }
