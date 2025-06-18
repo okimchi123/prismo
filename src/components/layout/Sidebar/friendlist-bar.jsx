b678d016835d299ab2b6c7caee9255075826f011
Okimchi <basilio.miko123@gmail.com>
2025-06-18 23:22:41 +0800

    added responsiveness on friendlist sheet

--- src/components/layout/Sidebar/friendlist-bar.jsx
+++ src/components/layout/Sidebar/friendlist-bar.jsx
@@ -8,11 +8,16 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Image from "next/image";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export function FriendListBar() {
-  return (
+
+  const isMobile = useIsMobile();
+  
+  if(isMobile){
+    return (
     <Sheet>
-      <SheetTrigger>
+      <SheetTrigger className="absolute top-3 right-4">
         <Image src="/friends.svg" width="20" height="20" alt="icon" />
       </SheetTrigger>
       <SheetContent>
@@ -48,4 +53,35 @@ export function FriendListBar() {
       </SheetContent>
     </Sheet>
   );
+  }
+  return(
+    <section className="w-[500px] bg-white p-4">
+      <h1 className="prismo font-semibold mb-3">Friends</h1>
+      <div className="flex flex-col gap-3">
+            <div className="flex items-center gap-2">
+              <figure className="w-10 h-10 relative">
+                <Image
+                  src="/jake.jpg"
+                  fill
+                  alt="profile_pic"
+                  className="object-cover rounded-md"
+                />
+              </figure>
+              <span className="text-[18px] font-semibold">Jake The Dog</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <figure className="w-10 h-10 relative">
+                <Image
+                  src="/jake.jpg"
+                  fill
+                  alt="profile_pic"
+                  className="object-cover rounded-md"
+                />
+              </figure>
+              <span className="text-[18px] font-semibold">Jake The Dog</span>
+            </div>
+      </div>
+    </section>
+  )
+  
 }
