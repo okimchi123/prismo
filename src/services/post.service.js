8e83b3a402fe9aef5cb1980869652c5b81b84e31
Okimchi <basilio.miko123@gmail.com>
2025-06-24 20:44:13 +0800

    created a handlePostSubmit function

---
+++ src/services/post.service.js
@@ -0,0 +1,21 @@
+import {collection, addDoc, Timestamp} from 'firebase/firestore'
+import { db
+
+ } from '@/lib/firebase'
+
+export async function handlePostSubmit(postMessage, user){
+    
+    try {
+        const docRef = await addDoc(collection(db, 'posts'),
+        {
+            text: postMessage,
+            userId: user.uid,
+            userName: `${user.firstname} ${user.lastname}`,
+            createdAt: Timestamp.now(),
+        }
+    );
+    console.log("Created with id:", docRef.id)
+    } catch (error) {
+        console.error(error)
+    }
+}
