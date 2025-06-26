ba32bdae4f409e258b7e5a8aa7e9f1db5fa734c6
Okimchi <basilio.miko123@gmail.com>
2025-06-26 15:28:06 +0800

    integrated date-fns for posts date

--- src/components/layout/Dashboard/post.jsx
+++ src/components/layout/Dashboard/post.jsx
@@ -1,11 +1,15 @@
 import DisplayImage from "@/components/ui/display-image";
 import Image from "next/image";
+import { formatDistanceToNow } from "date-fns";
 
-export default function UserPost({post, loading}) {
-  
-  const postIcons = [{name:"like", style:"mr-1"},{name:"comment", style:""},{name:"share", style:""}];
-  if(loading) return <p>Loading posts...</p>
-  return ( 
+export default function UserPost({ post, loading }) {
+  const postIcons = [
+    { name: "like", style: "mr-1" },
+    { name: "comment", style: "" },
+    { name: "share", style: "" },
+  ];
+  if (loading) return <p>Loading posts...</p>;
+  return (
     <main className="bg-white w-full p-3 flex flex-col gap-2">
       <header>
         <figure className="flex gap-1">
@@ -13,22 +17,29 @@ export default function UserPost({post, loading}) {
           <div className="flex flex-col">
             <h1 className="text-[14px]">{post.userName}</h1>
             <p className="text-[12px] prismo">@{post.userUsername}</p>
-            <span className="text-gray-500 text-[13px]">1h</span>
+            <small className="text-gray-500 text-[13px]">
+              {formatDistanceToNow(post.createdAt.toDate(), {
+                addSuffix: true,
+              })}
+            </small>
           </div>
         </figure>
       </header>
       <article className="">
-        <p>
-          {post.text}
-        </p>
+        <p>{post.text}</p>
       </article>
 
       <footer className="flex">
-        {postIcons.map((icon)=>(
-          <Image key={icon.name} src={`/post/${icon.name}.svg`} width="35" height="35" alt={icon.name} className={icon.style} />
-        ))
-
-        }
+        {postIcons.map((icon) => (
+          <Image
+            key={icon.name}
+            src={`/post/${icon.name}.svg`}
+            width="35"
+            height="35"
+            alt={icon.name}
+            className={icon.style}
+          />
+        ))}
       </footer>
     </main>
   );
