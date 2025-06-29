6c3d5a92eb05a1f96ccab95d45708c68d1569770
Okimchi <basilio.miko123@gmail.com>
2025-06-29 19:51:00 +0800

    updated cube design

--- src/components/3D/cube.jsx
+++ src/components/3D/cube.jsx
@@ -1,8 +1,12 @@
 'use client'
 import { motion } from "framer-motion"
+import Image from "next/image"
 
 export function Cube(){
-    const sides = "bg-[#FFA1B3] h-full w-full absolute border-white border-2"
+    const sides = "bg-[#FFA1B3] flex items-center justify-center h-full w-full absolute border-white border-2"
+
+    const logo = <Image src="/icon.svg" width="75" height="75" alt="Loading..." className="" />;
+
     return(
         <motion.div animate={{
             rotateZ:[90,180,5,90],
@@ -10,14 +14,14 @@ export function Cube(){
             rotateY:[20,100,5,20]
         }}
         transition={{
-            duration:5, repeat: Infinity, repeatDelay: 0
-        }} className="transform-3d relative rounded-lg w-[200px] h-[200px]">
-              <motion.div className={`front ${sides} translate-z-[100px]`}></motion.div>  
-              <motion.div className={`back ${sides} -translate-z-[100px]`}></motion.div> 
-              <motion.div className={`left ${sides} -translate-x-[100px] rotate-y-90`}></motion.div>
-              <motion.div className={`right ${sides} translate-x-[100px] -rotate-y-90`}></motion.div>
-              <motion.div className={`top ${sides} -translate-y-[100px] rotate-x-90`}></motion.div>
-              <motion.div className={`bottom ${sides} translate-y-[100px] -rotate-x-90`}></motion.div>
+            duration:4, repeat: Infinity, repeatDelay: 0
+        }} className="transform-3d relative rounded-lg w-[150px] h-[150px]">
+              <motion.div className={`front ${sides} translate-z-[75px]`}> {logo} </motion.div>  
+              <motion.div className={`back ${sides} -translate-z-[75px]`}> {logo} </motion.div> 
+              <motion.div className={`left ${sides} -translate-x-[75px] rotate-y-90`}> {logo} </motion.div>
+              <motion.div className={`right ${sides} translate-x-[75px] -rotate-y-90`}> {logo} </motion.div>
+              <motion.div className={`top ${sides} -translate-y-[75px] rotate-x-90`}> {logo} </motion.div>
+              <motion.div className={`bottom ${sides} translate-y-[75px] -rotate-x-90`}> {logo}  </motion.div>
         </motion.div>
     )
 }
