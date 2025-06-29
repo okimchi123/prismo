dede055915b108ca56a98b73824d891280e2f0bd
Okimchi <basilio.miko123@gmail.com>
2025-06-29 19:50:52 +0800

    updated friendlist frontend

--- src/app/(main)/friends/page.jsx
+++ src/app/(main)/friends/page.jsx
@@ -1,10 +1,8 @@
-import { Cube } from "@/components/3D/cube" 
 
 export default function Page(){
     return(
         <main>
             <h1 className="mb-30">Friend Lists</h1>
-            <Cube />
         </main>
     )
 }
