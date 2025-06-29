45eaa2724d53381894dfffcf21b3f3428afc0bfe
Okimchi <basilio.miko123@gmail.com>
2025-06-29 19:51:11 +0800

    created initial design for comment modal

--- src/components/layout/modal/comment.jsx
+++ src/components/layout/modal/comment.jsx
@@ -1,7 +1,11 @@
-export function CommentModal(){
-    return(
-        <div>
-            
+export function CommentModal({ isOpen, Close }) {
+  return (
+    isOpen && (
+      <div className="fixed top-0 right-0 z-100 w-full h-screen bg-[#FFA1B3]/30 flex justify-center items-center">
+        <div className="w-[600px] h-[400px] bg-white">
+          <button onClick={Close}>Close</button>
         </div>
+      </div>
     )
+  );
 }
