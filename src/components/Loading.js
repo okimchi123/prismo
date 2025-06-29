0d9a4ac515d8d150a5a0e53980a7864af346c622
Okimchi <basilio.miko123@gmail.com>
2025-06-29 19:50:36 +0800

    updated the loading component to 3d

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import { Cube } from "./3D/cube";
+
 export default function Loading() {
   return (
     <div className="flex flex-col items-center mt-[40%] sm:mt-[20%] md:mt-[10%] h-screen">
-      <Image src="/icon.svg" width="100" height="100" alt="Loading..." className="" />
+      <Cube />
     </div>
   );
 }
