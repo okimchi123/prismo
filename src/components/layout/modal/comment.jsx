c6649ee88a8f7987ac2520cc496f2705dce51849
Okimchi <basilio.miko123@gmail.com>
2025-06-30 23:54:53 +0800

    updated comment modal design

--- src/components/layout/modal/comment.jsx
+++ src/components/layout/modal/comment.jsx
@@ -2,7 +2,7 @@ export function CommentModal({ isOpen, Close }) {
   return (
     isOpen && (
       <div className="fixed top-0 right-0 z-100 w-full h-screen bg-[#FFA1B3]/30 flex justify-center items-center">
-        <div className="w-[600px] h-[400px] bg-white">
+        <div className="w-[400px] h-[400px] bg-white">
           <button onClick={Close}>Close</button>
         </div>
       </div>
