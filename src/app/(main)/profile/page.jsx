ce827f4057523cf9c13582aea3754015e211b87f
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-07-06 00:19:42 +0800

    created profile page initial

---
+++ src/app/(main)/profile/page.jsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+    return (
+        <div className="flex border border-black w-full flex-col items-start justify-center h-screen">
+        <h1 className="text-2xl font-bold mb-4">Profile Page</h1>
+        <p className="text-gray-600">This is the profile page content.</p>
+        </div>
+    )
+}
