e4f9cd2c5ecdbca87c78d31b94561e0f74f00e4b
Okimchi <basilio.miko123@gmail.com>
2025-07-08 23:56:12 +0800

    starting to add edit on profile

--- src/app/(main)/profile/page.jsx
+++ src/app/(main)/profile/page.jsx
@@ -1,19 +1,36 @@
 "use client";
+import { Pencil, Camera } from "lucide-react";
 import { storeUser } from "@/hooks/state";
 import { OwnPost } from "@/components/layout/Profile/UserPost";
 import { useUserPosts } from "@/hooks/fetchUserPost";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function ProfilePage() {
   const user = storeUser((state) => state.user);
+  const [edit, setEdit] = useState(false);
+
   const { posts, loading } = useUserPosts(user?.uid);
 
   return (
     <div className="flex mt-[2%] rounded-lg flex-col items-start">
-      <section className="bg-white pt-1 pb-3 px-3 w-full mb-2 rounded-lg">
+      <section className="bg-white relative pt-1 pb-3 px-3 w-full mb-2 rounded-lg">
+        <Pencil onClick={()=>setEdit(!edit)} height="20" width="20" className="prismo absolute right-3 top-3 select-none hover:scale-115 transition-all cursor-pointer" />
         <h1 className="prismo font-semibold mb-2">{user.username}</h1>
         <figure className="flex items-start gap-4">
-          <div className="PROFILE-PIC yellow-bg h-[100px] w-[100px]"></div>
+
+          <div className="PROFILE-PIC relative yellow-bg h-[100px] w-[100px]">
+            {edit?
+            <div className="shadow absolute flex justify-center items-center w-full h-full bg-black/50"> 
+              <Camera height="30" width="30" className="hover:scale-120 transition-all text-white cursor-pointer hover:text-pink-300 " />
+            </div> : <></>
+            }
+            
+
+          </div>
+
           <div className="USER-DATA flex flex-col gap-2 h-[100px] py-2 justify-between">
+
             <h1 className="FULLNAME text-lg font-medium leading-4">
               {user.firstname} {user.lastname}
             </h1>
