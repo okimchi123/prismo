ea2bf5d0df18eef3fa84ac66964aa8e153546a6b
Okimchi <basilio.miko123@gmail.com>
2025-07-09 23:33:47 +0800

    integrated changePic hook and fixed ui for profile

---
+++ src/components/layout/Profile/UserData.jsx
@@ -0,0 +1,130 @@
+"use client";
+import { Pencil, Camera } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import clsx from "clsx";
+import Image from "next/image";
+import ChangePic from "@/services/profile-pic.service";
+
+export default function UserData({ user, posts }) {
+  const [file, setFile] = useState(null);
+  const [previewPic, setPreviewPic] = useState(null);
+  const [edit, setEdit] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const newFile = e.target.files[0];
+    setFile(newFile);
+    setPreviewPic(URL.createObjectURL(newFile));
+  };
+
+  const handleSave = async () => {
+    if (!file) return;
+    try {
+      setLoading(true);
+      await ChangePic(file, user.uid);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+      setEdit(false);
+      setPreviewPic(null)
+      setFile(null)
+    }
+  };
+
+  return (
+    <section className="bg-white relative pt-1 pb-3 px-3 w-full mb-2 rounded-lg">
+      <button
+        className="absolute right-3 top-3 select-none not-last:cursor-pointer"
+        onClick={() => setEdit(true)}
+        disabled={edit}
+      >
+        <Pencil
+          height="20"
+          width="20"
+          className={clsx(" transition-all", {
+            "text-gray-300": edit,
+            "prismo hover:scale-115": !edit,
+          })}
+        />
+      </button>
+
+      <h1 className="prismo font-semibold mb-2">{user.username}</h1>
+      <figure className="flex items-start gap-4">
+        <div className="PROFILE-PIC relative yellow-bg h-[100px] w-[100px]">
+          {edit ? (
+            <div className="shadow absolute flex justify-center items-center w-full h-full bg-black/50">
+              <label htmlFor="profileID" className="cursor-pointer">
+                {previewPic ? (
+                <figure className="w-18 h-18 relative">
+                    <Image
+                    src={previewPic}
+                    fill
+                    className="rounded-full object-cover"
+                    alt="newPic"
+                  />
+                </figure>
+                  
+                ) : (
+                  <Camera
+                    height="30"
+                    width="30"
+                    className="hover:scale-120 transition-all select-none text-white cursor-pointer hover:text-pink-400 "
+                  />
+                )}
+              </label>
+              <input
+                id="profileID"
+                type="file"
+                accept="image/*"
+                onChange={handleChange}
+                hidden
+              />
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+
+        <div className="USER-DATA flex flex-col gap-2 h-[100px] py-2 justify-between">
+          <h1 className="FULLNAME text-lg font-medium leading-4">
+            {user.firstname} {user.lastname}
+          </h1>
+          <div className="FRIENDS-POSTS flex gap-3">
+            <div className="flex flex-col items-center">
+              <p className="text-sm"> 3 </p>
+              <h2 className="leading-3 text-sm">friends</h2>
+            </div>
+            <div className="flex flex-col items-center">
+              <p className="text-sm"> {posts.length} </p>
+              <h2 className="leading-3 text-sm">posts</h2>
+            </div>
+          </div>
+        </div>
+      </figure>
+      {edit ? (
+        <div className="absolute right-3 bottom-3 select-none flex gap-2">
+          <Button
+            className="border-red-500 border-2 bg-white/0 text-red-500 hover:bg-red-500 hover:text-white hover:scale-110 transition-all active:scale-100"
+            onClick={() => setEdit(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={handleSave}
+            className={clsx(
+              "text-white  hover:scale-110 transition-all active:scale-100",
+              { "bg-prismo hover:bg-prismo": file }
+            )}
+            disabled={!file}
+          >
+            Save
+          </Button>
+        </div>
+      ) : (
+        <></>
+      )}
+    </section>
+  );
+}
