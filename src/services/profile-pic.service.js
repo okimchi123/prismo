f63dc59460381fed5e460aac21c83f389bb32c09
Okimchi <basilio.miko123@gmail.com>
2025-07-10 23:36:21 +0800

    refractor profile pic hook from firebase storage to cloudinary

--- src/services/profile-pic.service.js
+++ src/services/profile-pic.service.js
@@ -1,16 +1,27 @@
 import {doc, updateDoc } from 'firebase/firestore'
-import { db, storage } from '@/lib/firebase'
-import {ref, uploadBytes, getDownloadURL} from 'firebase/storage'
+import { db } from '@/lib/firebase'
 
 export default async function ChangePic( file, userID){
     if(!file) return;
-    const storageRef = ref(storage, `profile_pics/${userID}`)
-    await uploadBytes(storageRef, file);
-    const downloadURL = await getDownloadURL(storageRef)
 
-    const userRef = doc(db, 'users', user)
+    const formData = new FormData();
+    formData.append("file", file)
+    formData.append("upload_preset", "profile_pic_upload")
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/dw8mhxm3l/image/upload',{
+        method:"POST",
+        body:formData,
+    });
+
+    if(!res.ok) throw new Error("failed to upload image");
+
+    const data = await res.json()
+    const downloadURL = data.secure_url;
+
+    const userRef = doc(db, 'users', userID)
     await updateDoc(userRef,{
         dpURL: downloadURL,
     })
+
     return downloadURL;
 }
