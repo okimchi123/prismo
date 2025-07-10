13fedb0247b608e1db039ee6d104044e0c7c8d27
Okimchi <basilio.miko123@gmail.com>
2025-07-10 23:35:45 +0800

    added cloudinary to next config image

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     env: {
       JWT_SECRET: process.env.JWT_SECRET,
     },
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
   };
   
   export default nextConfig;
