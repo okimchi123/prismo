1f6481a46ef7119cfd8963551fdcfbcb464b508b
Okimchi <basilio.miko123@gmail.com>
2025-07-22 23:53:47 +0800

    starting custom profile pic

--- src/components/layout/modal/ProfileData.jsx
+++ src/components/layout/modal/ProfileData.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
-import {ChangePic, ChangeUserData} from "@/services/user-update";
+import { ChangePic, ChangeUserData } from "@/services/user-update";
 import clsx from "clsx";
 import EditUserData from "../Profile/EditUserData";
 
@@ -17,7 +17,7 @@ export default function ProfileData({ close, user }) {
     firstname: "",
     lastname: "",
     nickname: "",
-  })
+  });
 
   const handleChange = (e) => {
     const newFile = e.target.files[0];
@@ -27,14 +27,13 @@ export default function ProfileData({ close, user }) {
   const handleSave = async () => {
     setLoading(true);
     try {
-      if(file){
+      if (file) {
         toast.loading("Updating your profile...", { id: "load" });
         await ChangePic(file, user.uid);
       }
-      if(userData.firstname || userData.lastname || userData.nickname){
-        await ChangeUserData(user.uid, userData)
+      if (userData.firstname || userData.lastname || userData.nickname) {
+        await ChangeUserData(user.uid, userData);
       }
-
     } catch (error) {
       console.log(error);
     } finally {
@@ -78,20 +77,23 @@ export default function ProfileData({ close, user }) {
               />
             )}
           </figure>
-          <label
-            htmlFor="profileID"
-            className="text-sm gap-[2px] py-1 font-medium prismo cursor-pointer hover:scale-105 transition-all flex items-center"
-          >
-            <Camera size="18" />
-            Change Image
-          </label>
-          <input
-            id="profileID"
-            type="file"
-            accept="image/*"
-            onChange={handleChange}
-            hidden
-          />
+          <div className="border border-black relative">
+            <button className="text-sm gap-[2px] py-1 font-medium prismo cursor-pointer hover:scale-105 transition-all flex items-center">
+              <Camera size="18" />
+              Change Image
+            </button>
+            <motion.div className="absolute -bottom-13 bg-white w-full flex flex-col">
+              <span>Prismo Pics</span>
+              <label htmlFor="profileID">From Device</label>
+            </motion.div>
+            <input
+              id="profileID"
+              type="file"
+              accept="image/*"
+              onChange={handleChange}
+              hidden
+            />
+          </div>
         </section>
         <EditUserData user={user} setData={setUserData} userDatas={userData} />
         <div className="absolute right-3 bottom-3 select-none flex gap-2">
@@ -105,9 +107,21 @@ export default function ProfileData({ close, user }) {
             onClick={handleSave}
             className={clsx(
               "text-white  hover:scale-110 transition-all active:scale-100",
-              { "bg-prismo hover:bg-prismo": file || userData.firstname || userData.lastname || userData.nickname }
+              {
+                "bg-prismo hover:bg-prismo":
+                  file ||
+                  userData.firstname ||
+                  userData.lastname ||
+                  userData.nickname,
+              }
             )}
-            disabled={!userData.firstname && !userData.lastname && !userData.nickname && !file || loading}
+            disabled={
+              (!userData.firstname &&
+                !userData.lastname &&
+                !userData.nickname &&
+                !file) ||
+              loading
+            }
           >
             Save
           </Button>
