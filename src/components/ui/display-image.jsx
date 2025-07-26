abbd2341b10c2572ed12632fa400b6691e48ab82
Okimchi <basilio.miko123@gmail.com>
2025-07-26 17:47:53 +0800

    created new image display component

--- src/components/ui/display-image.jsx
+++ src/components/ui/display-image.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function DisplayImage({ img }) {
+export function DisplayImage({ img }) {
   return (
     <figure className="w-13 h-13 relative">
       <Image
@@ -10,5 +10,29 @@ export default function DisplayImage({ img }) {
         className="object-cover rounded-md"
       />
     </figure>
-  )
+  );
+}
+export function SmallDisplayImg({ img }) {
+  return (
+    <figure className="w-11 h-11 relative">
+      <Image
+        src={img}
+        fill
+        alt="profile_pic"
+        className="object-cover rounded-md"
+      />
+    </figure>
+  );
+}
+export function CommentImage({ img }) {
+  return (
+    <figure className="w-10 h-10 relative">
+      <Image
+        src={img}
+        fill
+        className="rounded-full object-cover"
+        alt="pic"
+      />
+    </figure>
+  );
 }
