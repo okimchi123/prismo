b422cfbd2d9ad2ff1290097a169d95b894fb9e3b
Okimchi <basilio.miko123@gmail.com>
2025-07-31 23:57:26 +0800

    connected profileuserdata to profile/userdata component

--- src/components/layout/Profile/UserData.jsx
+++ src/components/layout/Profile/UserData.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import ProfileData from "../modal/ProfileData";
 import { AnimatePresence } from "framer-motion";
 import { EditImage } from "@/components/ui/display-image";
+import ProfileUserData from "@/components/ui/ProfileUserData";
 
 export default function UserData({ user, posts }) {
   const [editModal, setEditModal] = useState(false);
@@ -32,33 +33,7 @@ export default function UserData({ user, posts }) {
           />
         </button>
         <h1 className="prismo font-semibold mb-2">{user.username}</h1>
-        <figure className="flex items-start gap-4">
-          <div className="PROFILE-PIC flex justify-center items-center relative yellow-bg h-[100px] w-[100px]">
-            <figure className="w-18 h-18 relative">
-              {user.localPic ? (
-                <EditImage img={user.localPic} />
-              ) : (
-                <EditImage img={user.dpURL} />
-              )}
-            </figure>
-          </div>
-
-          <div className="USER-DATA flex flex-col gap-2 h-[100px] py-2 justify-between">
-            <h1 className="FULLNAME text-lg font-medium leading-4">
-              {user.firstname} {user.lastname}
-            </h1>
-            <div className="FRIENDS-POSTS flex gap-3">
-              <div className="flex flex-col items-center">
-                <p className="text-sm"> 3 </p>
-                <h2 className="leading-3 text-sm">friends</h2>
-              </div>
-              <div className="flex flex-col items-center">
-                <p className="text-sm"> {posts.length} </p>
-                <h2 className="leading-3 text-sm">posts</h2>
-              </div>
-            </div>
-          </div>
-        </figure>
+        <ProfileUserData user={user} posts={posts}/>
       </section>
     </>
   );
