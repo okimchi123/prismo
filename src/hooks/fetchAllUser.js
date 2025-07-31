0693a58a9739d0796b3c2f32b691ded44e37de98
Okimchi <basilio.miko123@gmail.com>
2025-07-31 23:55:45 +0800

    updated fetchallser hook added uid

--- src/hooks/fetchAllUser.js
+++ src/hooks/fetchAllUser.js
@@ -3,6 +3,9 @@ import { db } from "@/lib/firebase";
 
 export async function getAllUsers(){
     const querySnapshot = await getDocs(collection(db, 'users'));
-    const data = querySnapshot.docs.map(doc => doc.data())
+    const data = querySnapshot.docs.map(doc => ({
+    uid: doc.id,          
+    ...doc.data()        
+  }));
     return data
 }
