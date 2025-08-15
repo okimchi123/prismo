4288ec3f63b0dd806a81a807d8abfc2107b9c672
Okimchi <basilio.miko123@gmail.com>
2025-08-15 23:43:22 +0800

    fixed animation to accept friend request

--- src/components/layout/Sidebar/friend-request.jsx
+++ src/components/layout/Sidebar/friend-request.jsx
@@ -6,32 +6,42 @@ import { motion } from "framer-motion";
 
 export default function FriendRequestCard({ user }) {
   const [hover, setHover] = useState(false);
+  const [modal, setModal] = useState(false);
   const acceptIcon = {
     enter: {
       transform: "translateX(0px)",
       opacity: 1,
-      transition: { duration: 0.5 },
+      transition: { duration: 0.3 },
     },
     exit: {
       transform: "translateX(28px)",
       opacity:0,
-      transition: { duration: 0.5 },
+      transition: { duration: 0.3 },
     },
   };
   const declineIcon = {
     enter: {
       transform: "translateX(0px)",
       opacity: 1,
-      transition: { duration: 0.5 },
+      transition: { duration: 0.3 },
     },
     exit: {
       transform: "translateX(-28px)",
       opacity:0,
-      transition: { duration: 0.5 },
+      transition: { duration: 0.3 },
     },
   };
 
-  console.log(user);
+  const mouseLeave = () => {
+    setModal(false)
+    setTimeout(()=>{setHover(false)},300)
+  }
+
+  const mouseEnter = () => {
+    setHover(true)
+    setModal(true)
+  }
+
   return (
     <figure className="flex justify-between items-center p-1">
       <div className="flex gap-1 items-start">
@@ -43,22 +53,22 @@ export default function FriendRequestCard({ user }) {
         <h1 className="text-md"> {user.username} </h1>
       </div>
       <div
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
         className="flex gap-1"
       >
         {hover ? (
           <>
             <motion.div
             initial="exit"
-            animate={hover ? "enter" : "exit"}
+            animate={modal ? "enter" : "exit"}
             variants={acceptIcon}
             className="border p-2 rounded-md cursor-pointer bg-green-400 hover:bg-green-500 transition-all">
               <Check size="23" color="white" />
             </motion.div>
             <motion.div 
             initial="exit"
-            animate={hover ? "enter" : "exit"}
+            animate={modal ? "enter" : "exit"}
             variants={declineIcon}
             className="border p-2 rounded-md cursor-pointer bg-red-400 hover:bg-red-500 transition-all">
               <Circle size="21" color="white" />
