61d4d3c98a10f922b178f1f5c011193370fcdd32
Okimchi <basilio.miko123@gmail.com>
2025-08-16 00:19:42 +0800

    updated the fetch hooks and added reqID

--- src/hooks/FriendRequest.js
+++ src/hooks/FriendRequest.js
@@ -1,28 +1,35 @@
-'use client'
-import { useState, useEffect } from "react"
-import { fetchPendingRequests, fetchSenderUser } from "./Friend"
+"use client";
+import { useState, useEffect } from "react";
+import { fetchPendingRequests, fetchSenderUser } from "./Friend";
 
-export default function FriendRequest(userID){
-    const [requester, setRequester] = useState([])
-    const [senderProfiles, setSenderProfiles] = useState([])
-    useEffect(()=>{
-        async function getRequest(){
-            setRequester(await fetchPendingRequests(userID))
-        }
-        if(userID){
-          getRequest()  
-        }
-          
-    }, [userID])
+export default function FriendRequest(userID) {
+  const [requester, setRequester] = useState([]);
+  const [senderProfiles, setSenderProfiles] = useState([]);
+  useEffect(() => {
+    async function getRequest() {
+      setRequester(await fetchPendingRequests(userID));
+    }
+    if (userID) {
+      getRequest();
+    }
+  }, [userID]);
 
-    useEffect(()=>{
-        async function getRequester(){
-            const senderUids = requester.map(req => req.userID)
-            setSenderProfiles(await Promise.all(senderUids.map(fetchSenderUser)))
-        }
-        if(requester){
-            getRequester()
-        }
-    },[requester])
-    return senderProfiles
+  useEffect(() => {
+    async function getRequester() {
+      const senderData = requester.map((req) => ({
+        user: req.userID,
+        reqID: req.id,
+      }));
+      const profiles = await Promise.all(
+        senderData.map((data) => fetchSenderUser(data.user, data.reqID))
+      );
+
+      setSenderProfiles(profiles);
+    }
+    if (requester) {
+      getRequester();
+    }
+  }, [requester]);
+
+  return senderProfiles;
 }
