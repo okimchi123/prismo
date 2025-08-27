0504b323853024e99459b8c9c2f0f90afdc134ed
Okimchi <basilio.miko123@gmail.com>
2025-08-27 22:45:00 +0800

    created new modal for unfriend

---
+++ src/components/layout/User-Visit/Unfriend-Modal.jsx
@@ -0,0 +1,8 @@
+
+export default function UnfriendModal({toggle}) {
+  if(toggle){ 
+  return <div className="absolute z-200 p-2 bg-white w-20">
+        <h1>Unfriend</h1>
+        </div>
+         } 
+}
