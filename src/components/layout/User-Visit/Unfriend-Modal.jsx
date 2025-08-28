81289adff33a57142cea32fc79af6c42428ebbba
Okimchi <basilio.miko123@gmail.com>
2025-08-28 20:06:45 +0800

    finalized the unfriend modal design and animation

--- src/components/layout/User-Visit/Unfriend-Modal.jsx
+++ src/components/layout/User-Visit/Unfriend-Modal.jsx
@@ -1,8 +1,20 @@
+import { motion, AnimatePresence } from "framer-motion";
 
-export default function UnfriendModal({toggle}) {
-  if(toggle){ 
-  return <div className="absolute z-200 p-2 bg-white w-20">
-        <h1>Unfriend</h1>
-        </div>
-         } 
+export default function UnfriendModal({ toggle }) {
+  const PicAnimate = {};
+  return (
+    <AnimatePresence>
+  {toggle && (
+    <motion.div
+      onClick={() => console.log("pindot")}
+      initial={{ opacity: 0, y: -7 }}
+      animate={{ opacity: 1, y: 0, transition: { duration: 0 } }}
+      exit={{ opacity: 0, y: -7, transition: { duration: 0.20 } }}
+      className="absolute z-200 mt-2 py-2 px-5 rounded-md shadow-lg bg-white hover:bg-pink-100 transition-all"
+    >
+      <h1 className="select-none">Unfriend</h1>
+    </motion.div>
+  )}
+</AnimatePresence>
+  );
 }
