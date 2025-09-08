7bb3a77b3326236ac676376b8c62750886a92c4a
Okimchi <basilio.miko123@gmail.com>
2025-09-08 23:29:49 +0800

    updated backend

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -3,15 +3,20 @@ import { collection, addDoc, Timestamp, query, orderBy, onSnapshot } from "fireb
 import { db } from "@/lib/firebase"
 import { useState, useEffect } from "react";
 
-export async function addComment(postID, commentData, userID, userName){
+export async function addComment(postID, commentData, userID, userName, setLoading){
     const commentRef = collection(db, "posts", postID, "comments");
-
-    await addDoc(commentRef, {
+    try {
+        await addDoc(commentRef, {
         userID,
         userName,
         commentData,
         createdAt: Timestamp.now(),
     })
+    } catch (error) {
+        console.log(error)
+    }finally{
+        setLoading(false)
+    }
 }
 
 export function getComment(postID){
