2251ab884a45102d30cb8c31bc99050e90fff948
Okimchi <basilio.miko123@gmail.com>
2025-09-12 01:38:08 +0800

    integrated the new post hook

--- src/components/layout/modal/post.jsx
+++ src/components/layout/modal/post.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { storeUser, userFriends } from "@/hooks/state";
 import { Textarea } from "@/components/ui/textarea";
 import clsx from "clsx";
-import { handlePostSubmit } from "@/services/post.service";
+import { handlePostSubmit, handlePostSubmitWithFile } from "@/services/post.service";
 import { toast } from "sonner";
 import { motion } from "framer-motion";
 import { DisplayImage } from "@/components/ui/display-image";
@@ -15,6 +15,7 @@ import GifSearch from "./Gif";
 export default function PostModal({ onClose }) {
   const [gifModal, setGifModal] = useState(false);
   const [file, setFile] = useState(null);
+  const [gif, setGif] = useState(null);
   const [previewPic, setPreviewPic] = useState(null);
   const friends = userFriends((state) => state.friend);
   const user = storeUser((state) => state.user);
@@ -29,13 +30,18 @@ export default function PostModal({ onClose }) {
 
   const removePic = () => {
     setFile(null);
+    setGif(null)
     setPreviewPic(null);
   };
 
   const handlePost = async (e) => {
     e.preventDefault();
     if (!postMessage.trim()) return;
-    handlePostSubmit(postMessage, user, friends);
+    if(file){
+      handlePostSubmitWithFile(postMessage, user, friends, file)
+    }else{
+      handlePostSubmit(postMessage, user, friends, gif);
+    }
     toast.success("Post Created");
     setPostMessage("");
     onClose();
@@ -46,8 +52,7 @@ export default function PostModal({ onClose }) {
       exit={{ opacity: 0 }}
       className="absolute w-full md:fixed top-0 right-0 h-screen z-103 flex justify-center items-start md:items-center bg-[#FFA1B3]/30"
     >
-      <motion.form
-        key="modal"
+      <motion.div
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
         exit={{ scale: 0 }}
@@ -138,10 +143,10 @@ export default function PostModal({ onClose }) {
               <img src="/icons/gif.svg" alt="gificon" className="prismo" />
               <span>GIF</span>
             </div>
-            {gifModal && <GifSearch setGifModal={setGifModal} setPreviewPic={setPreviewPic} />}
+            {gifModal && <GifSearch setGifModal={setGifModal} setPreviewPic={setPreviewPic} setGif={setGif} />}
           </div>
         </section>
-      </motion.form>
+      </motion.div>
     </motion.main>
   );
 }
