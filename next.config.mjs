24ad3d1c687a66d227ac6f778e6c7171ab837cfe
Okimchi <basilio.miko123@gmail.com>
2025-09-11 18:50:39 +0800

    added new domain to nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      JWT_SECRET: process.env.JWT_SECRET,
-    },
-    images: {
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
+  images: {
+    domains: ["media.tenor.com"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
   },
-  };
-  
-  export default nextConfig;
-  
+};
+
+export default nextConfig;
