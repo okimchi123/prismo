942b6985c364bd35d0edb0212e123be602f6bc9e
Okimchi <basilio.miko123@gmail.com>
2025-09-19 23:52:42 +0800

    fixed chat ui

--- src/components/layout/Sidebar/Message.jsx
+++ src/components/layout/Sidebar/Message.jsx
@@ -1,10 +1,14 @@
 "use client";
 import Image from "next/image";
-import { X } from "lucide-react";
+import { SendHorizonal, X } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 export default function Message({ currentUser, chatUser, close }) {
+    const [textQuery, setTextQuery] = useState("")
+
   return (
-    <div className="fixed w-[270px] h-[330px] bottom-3 right-8 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg">
+    <div className="fixed flex flex-col justify-between w-[270px] h-[330px] bottom-3 right-8 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg">
       <div className="flex justify-between items-center w-full p-2 shadow-[0_3px_6px_rgb(0,0,0,0.1)]">
         <div className="flex gap-2 items-center">
           <figure className="w-9 h-9 relative">
@@ -25,8 +29,19 @@ export default function Message({ currentUser, chatUser, close }) {
         </div>
          <button onClick={close} className="cursor-pointer hover:scale-107 transition-all"><X size="22" /></button>
       </div>
-      <section></section>
-      <div></div>
+      <section>
+        chat
+      </section>
+      <div className="flex items-center gap-2 p-2 w-full justify-between">
+        <textarea 
+        onChange={(e)=>setTextQuery(e.target.value)}
+        className="h-[40px] w-full text-[14px] rounded-md resize-none p-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]" placeholder="type something" />
+        <button 
+        hidden={!textQuery}
+        className="hover:scale-107 transition-all cursor-pointer">
+            <SendHorizonal className="prismo" size="22" />
+        </button>
+      </div>
     </div>
   );
 }
