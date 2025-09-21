d07f680d7a80d0cb7adc99539f0d8da07978c9f7
Okimchi <basilio.miko123@gmail.com>
2025-09-21 22:14:18 +0800

    updated commnet backend

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -1,42 +1,56 @@
-'use client'
-import { collection, addDoc, Timestamp, query, orderBy, onSnapshot } from "firebase/firestore" 
-import { db } from "@/lib/firebase"
+"use client";
+import {
+  collection,
+  addDoc,
+  Timestamp,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "@/lib/firebase";
 import { useState, useEffect } from "react";
 
-export async function addComment(postID, commentData, userID, userName, setLoading){
-    const commentRef = collection(db, "posts", postID, "comments");
-    try {
-        await addDoc(commentRef, {
-        userID,
-        userName,
-        commentData,
-        createdAt: Timestamp.now(),
-    })
-    } catch (error) {
-        console.log(error)
-    }finally{
-        setLoading(false)
-    }
+export async function addComment(
+  postID,
+  commentData,
+  user,
+  setLoading
+) {
+  const commentRef = collection(db, "posts", postID, "comments");
+  try {
+    await addDoc(commentRef, {
+      userID: user.uid,
+      userName: user.username,
+      commentData,
+      userDP: user.dpURL,
+      userLocalPic: user.localPic,
+      createdAt: Timestamp.now(),
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setLoading(false);
+  }
 }
 
-export function getComment(postID){
-    const [comment, setComment] = useState([]);
+export function getComment(postID) {
+  const [comment, setComment] = useState([]);
 
-    useEffect(()=>{
-        if(!postID) return;
+  useEffect(() => {
+    if (!postID) return;
 
-        const commentRef = collection(db, "posts", postID, "comments");
-        const q = query(commentRef, orderBy("createdAt", "desc"))
+    const commentRef = collection(db, "posts", postID, "comments");
+    const q = query(commentRef, orderBy("createdAt", "desc"));
 
-        const unsubscribe = onSnapshot(q, (snapshot) =>{
-            const data = snapshot.docs.map((doc)=>({
-                id: doc.id,
-                ...doc.data(),
-            }))
-            setComment(data)
-        })
-        return () => unsubscribe();
-     },[postID])
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setComment(data);
+    });
+    return () => unsubscribe();
+  }, [postID]);
 
-     return comment;
+  return comment;
 }
